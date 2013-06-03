fb6e095744b92ffa33df4bc5d78cee8dec9863f7
Agustin Marseillan <agustinmarseillan@gmail.com>
2013-06-03 10:26:36 -0300

    conragay

--- olapTP/src/main/java/olap/web/controller/AutomaticOutputController.java
+++ olapTP/src/main/java/olap/web/controller/AutomaticOutputController.java
@@ -6,13 +6,13 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import olap.model.DBColumn;
+import olap.api.SpatialOlapApi;
+import olap.api.SpatialOlapApiSingletonImpl;
+import olap.db.DBColumn;
+import olap.db.MultiDimMapper;
+import olap.db.SingleTable;
 import olap.model.MultiDim;
-import olap.model.MultiDimConverter;
-import olap.model.MultiDimConverterDummy;
-import olap.model.SingleTable;
-import olap.model.SpatialOlapApi;
-import olap.model.SpatialOlapApiImpl;
+import olap.model.TypeHelper;
 import olap.repository.TablesRepository;
 import olap.repository.impl.TablesDatabaseRepository;
 
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -33,35 +32,43 @@ public class AutomaticOutputController {
 	}
 
 	@RequestMapping(value = "/createAutomaticOutput", method = RequestMethod.POST)
-	protected ModelAndView generateAutomaticOutput(@RequestParam MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
-		TablesRepository tablesDAO = TablesDatabaseRepository.getInstance();
+	protected ModelAndView generateAutomaticOutput(@RequestParam String file, HttpServletRequest request, HttpServletResponse response) {
+		TablesRepository tablesRepository = TablesDatabaseRepository.getInstance();
 		
-		SpatialOlapApi api = SpatialOlapApiImpl.getInstance();
+		SpatialOlapApi api = SpatialOlapApiSingletonImpl.getInstance();
 		
-		MultiDim multidim = api.getMultiDim(file);
+		MultiDim multidim = api.getMultiDim("input.xml");
 		List<DBColumn> multidimColumns = multidim.getColumns();
 		
-		List<MultiDimConverter> columnsInTable = new LinkedList<MultiDimConverter>();
+		List<MultiDimMapper> columnsInTable = new LinkedList<MultiDimMapper>();
 		
-		for(DBColumn multidimColumn : multidimColumns) {
-			MultiDimConverterDummy dic = new MultiDimConverterDummy(multidimColumn.getName());
+		for(DBColumn col : multidimColumns) {
+			MultiDimMapper dic = new MultiDimMapper(col.getName(), null);
 			columnsInTable.add(dic);
 		}
 		ModelAndView mav = new ModelAndView("manageSelectedColumns");
+		
 		mav.addObject("columnsInTable", columnsInTable);
 		
-		String tableName = multidim.getCubos().get(0).getName();
+		String tableName = multidim.getOlapCubes().get(0).getName();
 		SingleTable table = createTable(tableName, multidimColumns);
 		
-		tablesDAO.createTable(table);
+		tablesRepository.createTable(table);
 		
-//		URL input = getClass().getClassLoader().getResource("input.xml");
-//		String inputPath = input.toString();
-//		String path = inputPath.substring(0, inputPath.lastIndexOf("/"));
 		api.generateOutput("geomondrian.xml", columnsInTable, multidim, tableName);
 		
-		mav.addObject("message",  "Su archivo est&aacute; listo");
-		
 		return mav;
+		
+	}
+	
+	private SingleTable createTable(String tableName, List<DBColumn> columns) {
+		List<DBColumn> tableColumns = new LinkedList<DBColumn>();
+		for(DBColumn column : columns) {
+			String type = TypeHelper.toDBType(column.getType());
+			DBColumn tableColumn = new DBColumn(column.getName(), type, column.isPrimaryKey(), column.isNotNull());
+			tableColumns.add(tableColumn);
+		}
+		SingleTable table = new SingleTable(tableName, tableColumns);
+		return table;
 	}
 }
